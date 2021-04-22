0bf351e0a7c210338a587c6ef711254bc0980cd4
Byuntil <gugugu2014@jbnu.ac.kr>
2021-04-22 10:50:07 +0900

    .

--- Prototype/Prototype/Window1.xaml.cs
+++ Prototype/Prototype/Window1.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Prototype.views
+namespace Prototype
 {
     /// <summary>
-    /// UserControl1.xaml에 대한 상호 작용 논리
+    /// Window1.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class UserControl1 : UserControl
+    public partial class Window1 : Window
     {
-        public UserControl1()
+        public Window1()
         {
             InitializeComponent();
         }
