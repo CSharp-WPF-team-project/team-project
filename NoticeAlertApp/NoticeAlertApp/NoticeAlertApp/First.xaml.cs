905df3d0c9e45f5a383f76d910bcbafd604a5f2b
Y.jin <syeon_k112@naver.com>
2021-05-09 11:55:31 +0900

    메트리얼도입+메뉴부분수정
    자세한 사항은 notion 확인 부탁드립니다.

---
+++ NoticeAlertApp/NoticeAlertApp/NoticeAlertApp/First.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NoticeAlertApp
+{
+    /// <summary>
+    /// First.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class First : Window
+    {
+        public First()
+        {
+            InitializeComponent();
+        }
+
+        //메뉴 조정 버튼들
+        private void OpenMenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenMenuButton.Visibility = Visibility.Collapsed;
+            CloseMenuButton.Visibility = Visibility.Visible;
+        }
+        private void CloseMenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenMenuButton.Visibility = Visibility.Visible;
+            CloseMenuButton.Visibility = Visibility.Collapsed;
+        }
+    }
+}
--- NoticeAlertApp/NoticeAlertApp/NoticeAlertApp/MainWindow.xaml.cs
+++ NoticeAlertApp/NoticeAlertApp/NoticeAlertApp/MainWindow.xaml.cs
@@ -10,15 +10,27 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace NoticeApp
+namespace NoticeAlertApp
 {
-    public partial class AfterLoginFirst : Window
+
+    public partial class MainWindow : Window
     {
-        public AfterLoginFirst()
+        public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void SignInButton_Click(object sender, RoutedEventArgs e)
+        {
+            //First 창으로 넘어가게 해야함.   
         }
     }
 }
