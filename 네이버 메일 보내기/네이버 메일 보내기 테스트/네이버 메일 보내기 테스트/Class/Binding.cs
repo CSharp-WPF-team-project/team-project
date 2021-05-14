e07d128706e3bffd6ee0fd9afa4213c0fc181558
Byuntil <gugugu2014@jbnu.ac.kr>
2021-05-15 02:44:42 +0900

    준비 단계

---
+++ 네이버 메일 보내기/네이버 메일 보내기 테스트/네이버 메일 보내기 테스트/Class/Binding.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 네이버_메일_보내기_테스트.Class
+{
+    public class Binding
+    {
+        
+    }
+}
---
+++ 네이버 메일 보내기/네이버 메일 보내기 테스트/네이버 메일 보내기 테스트/View/Main.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace 네이버_메일_보내기_테스트.View
+{
+    /// <summary>
+    /// Main.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Main : Window
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+    }
+}
