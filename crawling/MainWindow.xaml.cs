694ef5f4b16bb4d783f7b8f1e415ea65b575c70f
nohjunh <nsh042777@naver.com>
2021-05-15 23:13:10 +0900

    강의레포트, 강의공지 추가

--- crawling/MainWindow.xaml.cs
+++ crawling/MainWindow.xaml.cs
@@ -36,6 +36,7 @@ namespace crawling
 		protected ChromeOptions _options = null;
 		protected ChromeDriver _driver = null;
 
+		int countBtn1 = 0; // 처음 실행이 아님을 확인
 		int countBtn2 = 0; // 처음 실행이 아님을 확인
 
 		public MainWindow()
@@ -44,6 +45,9 @@ namespace crawling
 
 			button1.Click += button1_Initialized;
 			button2.Click += button2_Initialized;
+			button3.Click += button3_Initialized;
+			button4.Click += button4_Initialized;
+			button5.Click += button5_Initialized;
 
 			_driverService = ChromeDriverService.CreateDefaultService();
 			_driverService.HideCommandPromptWindow = true;
@@ -56,9 +60,53 @@ namespace crawling
 
 		}
 
+
+		// ------------------LMS로그인 확인--------------------------
 		public void button1_Initialized(object sender, EventArgs e)
 
 		{
+			var viewModel = this.DataContext as LoginViewModel;
+
+			_driver = new ChromeDriver(_driverService, _options);
+
+			_driver.Navigate().GoToUrl("https://ieilms.jbnu.ac.kr/"); // 웹 사이트에 접속합니다.
+
+			_driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+			IWebElement element;
+			try
+			{
+				element = _driver.FindElementByXPath("//*[@id='id']");
+				element.SendKeys(viewModel.LoginID);
+
+				element = _driver.FindElementByXPath("//*[@id='passwd']");
+				element.SendKeys(viewModel.LoginPasswd);
+
+				element = _driver.FindElementByXPath("//*[@id='loginform']/table/tbody/tr[1]/td[2]/input");
+				element.Click();
+
+				element = _driver.FindElementByXPath("//*[@id='boardAbox']/form/table/tbody/tr[1]/td[2]");
+				MessageBox.Show("로그인 성공! 원하는 메뉴를 클릭해주세요");
+
+				_driver.Close();
+			}
+			catch (Exception)
+			{
+				MessageBox.Show("ID,PW를 확인해주세요.");
+				return;
+			}
+		}
+
+
+		// ------------------강의자료 긁어오기--------------------------
+		public void button2_Initialized(object sender, EventArgs e)
+		{
+			if (countBtn1 != 0)
+			{
+				L_Data.Clear();
+			}
+			countBtn1++;
+
 			var viewModel = this.DataContext as LoginViewModel;
 
 			_driver = new ChromeDriver(_driverService, _options);
@@ -111,9 +159,9 @@ namespace crawling
 							string BASE_value = url;
 							element = _driver.FindElementByXPath(BASE_value);
 							element.Click();
-							TextUpLoad();
+							TextUpLoad2();
 						}
-						LmsCrawlingData.ItemsSource = L_Data;
+						Lms2CrawlingData.ItemsSource = L_Data;
 					}
 					if (Chkbox.Content.ToString() == "18.5학점")
 					{
@@ -126,9 +174,9 @@ namespace crawling
 							string BASE_value = url;
 							element = _driver.FindElementByXPath(BASE_value);
 							element.Click();
-							TextUpLoad();
+							TextUpLoad2();
 						}
-						LmsCrawlingData.ItemsSource = L_Data;
+						Lms2CrawlingData.ItemsSource = L_Data;
 					}
 					if (Chkbox.Content.ToString() == "15.5학점")
 					{
@@ -141,23 +189,17 @@ namespace crawling
 							string BASE_value = url;
 							element = _driver.FindElementByXPath(BASE_value);
 							element.Click();
-							TextUpLoad();
+							TextUpLoad2();
 						}
-						LmsCrawlingData.ItemsSource = L_Data;
+						Lms2CrawlingData.ItemsSource = L_Data;
 
 					}
 				}
 			}
-			// 전체 체크해제하기
-			foreach (CheckBox Chkbox in ChkBoxes)
-			{
-				Chkbox.IsChecked = false;
-			}
-
 			_driver.Close();
 		}
 
-		public void TextUpLoad()
+		public void TextUpLoad2()
 		{
 			try
 			{
@@ -165,7 +207,6 @@ namespace crawling
 				{
 					LmsSubject = _driver.FindElementByXPath("//*[@id='center']/div/div[1]/div[1]/div[1]").Text.Substring(9),
 					LmsTitle = _driver.FindElementByXPath("//*[@id='borderB']/tbody[2]/tr[1]/td[2]").Text,
-					LmsWriter = _driver.FindElementByXPath("//*[@id='borderB']/tbody[2]/tr[1]/td[3]").Text,
 					LmsRdate = _driver.FindElementByXPath("//*[@id='borderB']/tbody[2]/tr[1]/td[4]").Text,
 
 				});
@@ -182,7 +223,256 @@ namespace crawling
 			}
 		}
 
-		private void button2_Initialized(object sender, EventArgs e)
+		// ------------------강의레포트 긁어오기--------------------------
+		public void button3_Initialized(object sender, EventArgs e)
+		{
+			if (countBtn1 != 0)
+			{
+				L_Data.Clear();
+			}
+			countBtn1++;
+
+			var viewModel = this.DataContext as LoginViewModel;
+
+			_driver = new ChromeDriver(_driverService, _options);
+
+			_driver.Navigate().GoToUrl("https://ieilms.jbnu.ac.kr/"); // 웹 사이트에 접속합니다.
+
+			_driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+			IWebElement element;
+			try
+			{
+				element = _driver.FindElementByXPath("//*[@id='id']");
+				element.SendKeys(viewModel.LoginID);
+
+				element = _driver.FindElementByXPath("//*[@id='passwd']");
+				element.SendKeys(viewModel.LoginPasswd);
+
+				element = _driver.FindElementByXPath("//*[@id='loginform']/table/tbody/tr[1]/td[2]/input");
+				element.Click();
+
+				element = _driver.FindElementByXPath("//*[@id='boardAbox']/form/table/tbody/tr[1]/td[2]");
+				element.Click();
+			}
+			catch (Exception)
+			{
+				MessageBox.Show("ID,PW를 확인해주세요.");
+				return;
+			}
+
+			element = _driver.FindElementByXPath("//*[@id='nav']/li[4]/a");
+			element.Click();
+
+			// 학점선택 체크박스
+			IEnumerable<CheckBox> ChkBoxes = from checkbox in this.StackPanelGroup1.Children.OfType<CheckBox>()
+												 // where checkbox.IsChecked.Value 체크된 Checkbox 만 선택할때
+											 select checkbox;
+			// 체크된 content 값 가져오기
+			foreach (CheckBox Chkbox in ChkBoxes)
+			{
+				if (Chkbox.IsChecked == true)
+				{
+					if (Chkbox.Content.ToString() == "21.5학점")
+					{
+						for (int i = 2; i < 10; i++)
+						{
+							element = _driver.FindElementByXPath("//*[@id='center']/div/div[2]/div/div[3]/a/span");
+							element.Click();
+							string BASE_Path = "//*[@id='treeboxtab']/div/table/tbody/tr[{0}]/td[2]/table/tbody/tr/td[4]/span";
+							string url = string.Format(BASE_Path, i);
+							string BASE_value = url;
+							element = _driver.FindElementByXPath(BASE_value);
+							element.Click();
+							TextUpLoad3();
+						}
+						Lms3CrawlingData.ItemsSource = L_Data;
+					}
+					if (Chkbox.Content.ToString() == "18.5학점")
+					{
+						for (int i = 2; i < 9; i++)
+						{
+							element = _driver.FindElementByXPath("//*[@id='center']/div/div[2]/div/div[3]/a/span");
+							element.Click();
+							string BASE_Path = "//*[@id='treeboxtab']/div/table/tbody/tr[{0}]/td[2]/table/tbody/tr/td[4]/span";
+							string url = string.Format(BASE_Path, i);
+							string BASE_value = url;
+							element = _driver.FindElementByXPath(BASE_value);
+							element.Click();
+							TextUpLoad3();
+						}
+						Lms3CrawlingData.ItemsSource = L_Data;
+					}
+					if (Chkbox.Content.ToString() == "15.5학점")
+					{
+						for (int i = 2; i < 8; i++)
+						{
+							element = _driver.FindElementByXPath("//*[@id='center']/div/div[2]/div/div[3]/a/span");
+							element.Click();
+							string BASE_Path = "//*[@id='treeboxtab']/div/table/tbody/tr[{0}]/td[2]/table/tbody/tr/td[4]/span";
+							string url = string.Format(BASE_Path, i);
+							string BASE_value = url;
+							element = _driver.FindElementByXPath(BASE_value);
+							element.Click();
+							TextUpLoad3();
+						}
+						Lms3CrawlingData.ItemsSource = L_Data;
+
+					}
+				}
+			}
+			_driver.Close();
+		}
+
+		public void TextUpLoad3()
+		{
+			try
+			{
+				L_Data.Add(new LmsData()
+				{
+					LmsSubject = _driver.FindElementByXPath("//*[@id='center']/div/div[1]/div[1]/div[1]").Text.Substring(9),
+					LmsTitle = _driver.FindElementByXPath("//*[@id='borderB']/tbody/tr[2]/td[2]").Text,
+					LmsRdate = _driver.FindElementByXPath("//*[@id='borderB']/tbody/tr[2]/td[6]").Text
+
+				});
+			}
+			catch (Exception)
+			{
+				L_Data.Add(new LmsData()
+				{
+					LmsSubject = _driver.FindElementByXPath("//*[@id='center']/div/div[1]/div[1]/div[1]").Text.Substring(9),
+					LmsTitle = "업로드된 레포트가 없습니다."
+				});
+				return;
+
+			}
+		}
+
+
+		// ------------------강의공지 긁어오기--------------------------
+		public void button4_Initialized(object sender, EventArgs e)
+		{
+			if (countBtn1 != 0)
+			{
+				L_Data.Clear();
+			}
+			countBtn1++;
+
+			var viewModel = this.DataContext as LoginViewModel;
+
+			_driver = new ChromeDriver(_driverService, _options);
+
+			_driver.Navigate().GoToUrl("https://ieilms.jbnu.ac.kr/"); // 웹 사이트에 접속합니다.
+
+			_driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+			IWebElement element;
+			try
+			{
+				element = _driver.FindElementByXPath("//*[@id='id']");
+				element.SendKeys(viewModel.LoginID);
+
+				element = _driver.FindElementByXPath("//*[@id='passwd']");
+				element.SendKeys(viewModel.LoginPasswd);
+
+				element = _driver.FindElementByXPath("//*[@id='loginform']/table/tbody/tr[1]/td[2]/input");
+				element.Click();
+
+				element = _driver.FindElementByXPath("//*[@id='boardAbox']/form/table/tbody/tr[1]/td[2]");
+				element.Click();
+			}
+			catch (Exception)
+			{
+				MessageBox.Show("ID,PW를 확인해주세요.");
+				return;
+			}
+
+			element = _driver.FindElementByXPath("//*[@id='nav']/li[10]/a");
+			element.Click();
+
+			// 학점선택 체크박스
+			IEnumerable<CheckBox> ChkBoxes = from checkbox in this.StackPanelGroup1.Children.OfType<CheckBox>()
+												 // where checkbox.IsChecked.Value 체크된 Checkbox 만 선택할때
+											 select checkbox;
+			// 체크된 content 값 가져오기
+			foreach (CheckBox Chkbox in ChkBoxes)
+			{
+				if (Chkbox.IsChecked == true)
+				{
+					if (Chkbox.Content.ToString() == "21.5학점")
+					{
+						for (int i = 2; i < 10; i++)
+						{
+							string BASE_Path = "//*[@id='treebox']/div/table/tbody/tr[{0}]/td[2]/table/tbody/tr/td[4]/span";
+							string url = string.Format(BASE_Path, i);
+							string BASE_value = url;
+							element = _driver.FindElementByXPath(BASE_value);
+							element.Click();
+							Thread.Sleep(300);
+							TextUpLoad4();
+						}
+						Lms4CrawlingData.ItemsSource = L_Data;
+					}
+					if (Chkbox.Content.ToString() == "18.5학점")
+					{
+						for (int i = 2; i < 9; i++)
+						{
+							string BASE_Path = "//*[@id='treebox']/div/table/tbody/tr[{0}]/td[2]/table/tbody/tr/td[4]/span";
+							string url = string.Format(BASE_Path, i);
+							string BASE_value = url;
+							element = _driver.FindElementByXPath(BASE_value);
+							element.Click();
+							Thread.Sleep(300);
+							TextUpLoad4();
+						}
+						Lms4CrawlingData.ItemsSource = L_Data;
+					}
+					if (Chkbox.Content.ToString() == "15.5학점")
+					{
+						for (int i = 2; i < 8; i++)
+						{
+							string BASE_Path = "//*[@id='treebox']/div/table/tbody/tr[{0}]/td[2]/table/tbody/tr/td[4]/span";
+							string url = string.Format(BASE_Path, i);
+							string BASE_value = url;
+							element = _driver.FindElementByXPath(BASE_value);
+							element.Click();
+							Thread.Sleep(300);
+							TextUpLoad4();
+						}
+						Lms4CrawlingData.ItemsSource = L_Data;
+					}
+				}
+			}
+			_driver.Close();
+		}
+
+		public void TextUpLoad4()
+		{
+			try
+			{
+				L_Data.Add(new LmsData()
+				{
+					LmsSubject = _driver.FindElementByXPath("//*[@id='gname']").Text.Substring(9),
+					LmsTitle = _driver.FindElementByXPath("//*[@id='board']/tbody/tr[2]/td[2]/a").Text,
+					LmsRdate = _driver.FindElementByXPath("//*[@id='board']/tbody/tr[2]/td[3]").Text,
+
+				});
+			}
+			catch (Exception)
+			{
+				L_Data.Add(new LmsData()
+				{
+					LmsSubject = _driver.FindElementByXPath("//*[@id='gname']").Text.Substring(9),
+					LmsTitle = "업로드된 공지가 없습니다."
+				});
+			}
+		}
+
+
+
+
+		// ------------------학과공지 긁어오기--------------------------
+		private void button5_Initialized(object sender, EventArgs e)
 		{
 			if (countBtn2 != 0)
 			{
@@ -216,7 +506,7 @@ namespace crawling
 					D_Num = _driver.FindElementByXPath(Base_value1 + "td[@class='_artclTdNum']").Text,
 					D_Title = _driver.FindElementByXPath(Base_value1 + "td[@class='_artclTdTitle']").Text,
 					D_Writer = _driver.FindElementByXPath(Base_value1 + "td[@class='_artclTdWriter']").Text,
-					D_Rdate = _driver.FindElementByXPath(Base_value1 + "td[@class='_artclTdRdate']").Text,
+					D_Rdate = _driver.FindElementByXPath(Base_value1 + "td[@class='_artclTdRdate']").Text
 
 				});
 			}
