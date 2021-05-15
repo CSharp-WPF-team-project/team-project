afa8b4b93c7553018320ba6a839496178089b40d
Byuntil <gugugu2014@jbnu.ac.kr>
2021-05-15 21:06:40 +0900

    .

---
+++ 네이버 메일 보내기/네이버 메일 보내기 테스트/네이버 메일 보내기 테스트/Model/Binding.cs
@@ -0,0 +1,15 @@
+﻿namespace 네이버_메일_보내기_테스트.Class
+{
+    //메일 정보 -> 바인딩, 버튼 이벤트
+    public class Binding
+    {
+        private string mailAddress;
+
+        public string MailAddress
+        {
+            get { return mailAddress; }
+            set { mailAddress = value; }
+        }
+
+    }
+}
