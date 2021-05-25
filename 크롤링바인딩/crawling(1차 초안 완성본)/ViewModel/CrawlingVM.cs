f7ed571664c0a0791184dc1c16a60265cf89aea0
nohjunh <nsh042777@naver.com>
2021-05-25 15:45:19 +0900

    제출일시 수정

--- 크롤링바인딩/crawling(1차 초안 완성본)/ViewModel/CrawlingVM.cs
+++ 크롤링바인딩/crawling(1차 초안 완성본)/ViewModel/CrawlingVM.cs
@@ -12,6 +12,10 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 
 
+/// <summary>
+/// 제출일시 추가한 내역들까지 옮겨야 함.
+/// </summary>
+
 namespace crawling.ViewModel
 {
     class CrawlingVM
