661b58b6245605d9753c38434cdc250da7268840
nohjunh <nsh042777@naver.com>
2021-06-12 11:48:30 +0900

    Update Notice.xaml.cs

--- ★Notice 06.02/Notice/View/Notice.xaml.cs
+++ ★Notice 06.02/Notice/View/Notice.xaml.cs
@@ -17,40 +17,8 @@ namespace Notice.View
 		{
 			
 			InitializeComponent();
-			//click.Click += button1_Initialized;
 
 		}
-		
-		/*
-		public void button1_Initialized(object sender, EventArgs e)
-		{
-			
-			
-			// 학점선택 체크박스
-			IEnumerable<CheckBox> ChkBoxes = from checkbox in this.StackPanelGroup1.Children.OfType<CheckBox>()
-												 // where checkbox.IsChecked.Value 체크된 Checkbox 만 선택할때
-											 select checkbox;
-			// 체크된 content 값 가져오기
-			foreach (CheckBox Chkbox in ChkBoxes)
-			{
-				if (Chkbox.IsChecked == true)
-				{
-					if (Chkbox.Content.ToString() == "21.5학점")
-					{
-						//vm.grade.GradeNumber = 21;
-					}
-					if (Chkbox.Content.ToString() == "18.5학점")
-					{
-						//vm.grade.GradeNumber = 18;
-					}
-					if (Chkbox.Content.ToString() == "15.5학점")
-					{
-						//vm.grade.GradeNumber = 15;
-					}
-				}
-			}
-		}
-		*/
 
-	}
+    }
 }
